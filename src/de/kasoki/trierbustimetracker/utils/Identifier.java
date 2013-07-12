e901a1c6540a40f194d594a579973a21799c5ca7
Christopher Kaster <ikasoki@gmail.com>
2013-07-12 10:51:32 +0200

    remove unused attribute APP_PREFERENCES_FAVORITE_IDENTIFIER

--- src/de/kasoki/trierbustimetracker/utils/Identifier.java
+++ src/de/kasoki/trierbustimetracker/utils/Identifier.java
@@ -4,8 +4,7 @@ public class Identifier {
 	
 	public static final String APP_FAVORITE_FILE_IDENTIFIER = "de.kasoki.trierbustimetracker.CONF_FAVORITES";
 	public static final String APP_SETTINGS_FILE_IDENTIFIER = "de.kasoki.trierbustimetracker.SETTINGS";
-	public static final String APP_PREFERENCES_FAVORITE_IDENTIFIER = "de.kasoki.trierbustimetracker.FAVORITES";
-	
+
 	public static final String APP_SETTINGS_USE_NOTIFICATIONS_IDENTIFIER = "de.kasoki.trierbustimetracker.SettingsActivity.USE_NOTIFICATIONS";
 	public static final String APP_SETTINGS_USE_AUTO_RELOAD_IDENTIFIER = "de.kasoki.trierbustimetracker.SettingsActivity.USE_AUTO_RELOAD";
 
