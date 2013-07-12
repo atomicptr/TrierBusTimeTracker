1a4d304646d695bddf67d858f74726e0ff9bef5c
Christopher Kaster <ikasoki@gmail.com>
2013-07-12 10:56:56 +0200

    Revert "remove unused attribute APP_PREFERENCES_FAVORITE_IDENTIFIER"
    This reverts commit e901a1c6540a40f194d594a579973a21799c5ca7.

--- src/de/kasoki/trierbustimetracker/utils/Identifier.java
+++ src/de/kasoki/trierbustimetracker/utils/Identifier.java
@@ -4,7 +4,8 @@ public class Identifier {
 	
 	public static final String APP_FAVORITE_FILE_IDENTIFIER = "de.kasoki.trierbustimetracker.CONF_FAVORITES";
 	public static final String APP_SETTINGS_FILE_IDENTIFIER = "de.kasoki.trierbustimetracker.SETTINGS";
-
+	public static final String APP_PREFERENCES_FAVORITE_IDENTIFIER = "de.kasoki.trierbustimetracker.FAVORITES";
+	
 	public static final String APP_SETTINGS_USE_NOTIFICATIONS_IDENTIFIER = "de.kasoki.trierbustimetracker.SettingsActivity.USE_NOTIFICATIONS";
 	public static final String APP_SETTINGS_USE_AUTO_RELOAD_IDENTIFIER = "de.kasoki.trierbustimetracker.SettingsActivity.USE_AUTO_RELOAD";
 
